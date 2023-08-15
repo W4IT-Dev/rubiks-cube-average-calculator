9c283eba9c84d06099826af7c1ba550f88a35266
W4IT-Dev <w4it.dev.business@gmail.com>
2023-08-15 16:21:58 +0200

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,59 @@
+let times = [
+	{
+		time: 10
+	},
+	{
+		time: 50
+	},
+	{
+		time: 47
+	},
+	{
+		time: 23
+	},
+	{
+		time: 101
+	},
+	{
+		time: 22
+	},
+	{
+		time: 89
+	},
+	{
+		time: 73
+	},
+	{
+		time: 99
+	},
+	{
+		time: 13
+	},
+	{
+		time: 17
+	},
+	{
+		time: 44
+	},
+	{
+		time: 77
+	},
+	{
+		time: 88
+	},
+	{
+		time: 74
+	},
+	{
+		time: 39
+	},
+	{
+		time: 45
+	},
+	{
+		time: 09
+	},
+	{
+		time: 77
+	}
+];
